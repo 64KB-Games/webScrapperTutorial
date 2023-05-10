833ea4f2dd5d1b5d90dfb09990b0006063701b3a
Sanghyeon Baek <wink92@gmail.com>
2023-05-10 11:31:19 +0900

    tutorial complete

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"WebScrapper/scrapper"
+	"github.com/labstack/echo/v4"
+	"os"
+	"strings"
+)
+
+const fileName string = "jobs.csv"
+
+func main() {
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
+}
